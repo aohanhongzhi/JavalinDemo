6fa387b6206928d831aaded74e814f591cf050b5
aohanhongzhi <aohanhongzhi@qq.com>
2019-09-09 23:18:04 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# JavalinDemo
+## javalin
+#### 官网：https://javalin.io/
+
+
+
+## notes
+![](./asset/img/slf4j.png)
---
+++ src/main/java/hxy/javalin/JavalinDemo/App.java
@@ -0,0 +1,21 @@
+package hxy.javalin.JavalinDemo;
+
+import io.javalin.Javalin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("Hello World!" );
+        Javalin app = Javalin.create().start(7000);
+        app.get("/", ctx -> ctx.result("Hello World"));
+        
+        app.get("/hello/:name", ctx -> {
+            ctx.result("Hello: " + ctx.pathParam("name"));
+        });
+    }
+}
